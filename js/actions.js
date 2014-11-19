56b0d6a22761efabea3315850ac792efa7667f2f
KungD <david@krawaller.se>
2014-11-19 12:09:01 +0100

    major refactor

--- js/actions.js
+++ js/actions.js
@@ -1,15 +1,17 @@
-(function(Reflux, window) {
+(function(Reflux, global) {
     'use strict';
 
-    var Todo = Reflux.createActions([
-        "toggle",
-        "toggleAll",
-        "add",
-        "remove",
-        "clearCompleted",
-        "edit"
-    ]);
+    // Each action is like an event channel for one specific event. Actions are called by components.
+    // The store is listening to all actions, and the components in turn are listening to the store.
+    // Thus the flow is: User interaction -> component calls action -> store reacts and triggers -> components update
 
-    window.Todo = Todo;
+    global.TodoActions = Reflux.createActions([
+        "toggleItem",     // called by utton in TodoItem
+        "toggleAllItems", // called by button in TodoMain (even though you'd think TodoHeader)
+        "addItem",        // called by hitting enter in field in TodoHeader
+        "removeItem",     // called by button in TodoItem
+        "clearCompleted", // called by button in TodoFooter
+        "editItem"        // called by finishing edit in TodoItem
+    ]);
 
 })(window.Reflux, window);
